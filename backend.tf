ec2d2e36857f709fad82f03d1c0a022bb835bca7
shreya-161 <61554133+shreya-161@users.noreply.github.com>
2025-07-29 23:40:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "state-bucket" # change this
+    bucket         = "state-bucket"
     key            = "terraform/terraform.tfstate"
     region         = "us-east-1"
   }
